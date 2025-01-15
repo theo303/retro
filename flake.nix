c6915ec852ddd5ca9f7ef9806846bb9252abf854
theo303 <theo.coutant32@gmail.com>
2025-01-15 22:45:51 +0100

    feat: first commit

---
+++ flake.nix
@@ -0,0 +1,10 @@
+{
+  inputs = { nixpkgs.url = "github:NixOS/nixpkgs"; };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = import nixpkgs { inherit system; };
+      in with pkgs; {
+        devShells.default =
+          pkgs.mkShell { packages = [ protobuf protoc-gen-go go-task ]; };
+      });
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/theo303/retro
+
+go 1.23.3
+
+require google.golang.org/protobuf v1.36.3
